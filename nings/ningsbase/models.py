aa19fcdf58440dc2328d6429d55bf8af7d223dca
Raymond Feng <raymondhfeng@berkeley.edu>
2022-01-25 07:39:02 +0000

    chethan is a hoe

--- nings/ningsbase/models.py
+++ nings/ningsbase/models.py
@@ -1,21 +1,24 @@
 from django.db import models
 
-class Company (models.model):
+class Company (models.Model):
   name = models.CharField(max_length=256)
   size = models.CharField(max_length=256)
-  location = models.CharField(max_length=256)
+  address = models.CharField(max_length=256)
   ipo_status = models.CharField(max_length=256)
   funding_round = models.CharField(max_length=256)
 
-
-class Acquisition (models.model):
-  acquirer = models.ForeignKey(Company)
-  acquiree = models.ForeignKey(Company)
+class Acquisition (models.Model):
+  acquirer = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='company_acquirer')
+  acquiree = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='company_acquiree')
   amount = models.IntegerField()
+  
+class InvestingEntity(models.Model):
+  name = models.CharField(max_length=256)
+  investing_type = models.CharField(max_length=256)
 
-class Funding (models.model):
+class Funding (models.Model):
   funding_date = models.DateTimeField()
-  company = models.ForeignKey(Company)
-  investing_entity = models.CharField(max_length=256)
+  company = models.ForeignKey(Company, on_delete=models.CASCADE)
+  investing_entity = models.ForeignKey(InvestingEntity, on_delete=models.CASCADE)
   amount = models.IntegerField()
-  stage = models.Cha
+  stage = models.CharField(max_length=128)
